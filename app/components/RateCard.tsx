0a3caa3069a29671dc90f34b4dffbdaa5090eb89
Mikael <s.h.koo1213@gmail.com>
2024-02-19 14:33:07 -0500

    enable rate feature with loop to new list set

--- app/components/RateCard.tsx
+++ app/components/RateCard.tsx
@@ -1,19 +1,42 @@
 import './RateCard.css';
 
-const RateCard = ({ changeCurrent }) => {
-	function handleSubmit(e) {
+const RateCard = ({
+	changeCurrent,
+	current,
+	total,
+	addToNextList,
+	switchList
+}) => {
+	function handleClick(e) {
 		e.preventDefault();
+		const value = e.target.value;
+		const random = Math.random();
+
+		if (value === '1') {
+			if (random > 0.8) {
+				addToNextList(current - 1);
+			}
+		} else if (value === '2') {
+			if (random > 0.4) {
+				addToNextList(current - 1);
+			}
+		} else {
+			addToNextList(current - 1);
+		}
 		changeCurrent(1);
 	}
 
 	return (
-		<form
-			className="flex justify-center items-center gap-5 py-4"
-			onSubmit={handleSubmit}
-		>
-			<button>Excellent</button>
-			<button>Good</button>
-			<button>Bad</button>
+		<form className="flex justify-center items-center gap-5 py-4">
+			<button value={1} onClick={handleClick}>
+				Excellent
+			</button>
+			<button value={2} onClick={handleClick}>
+				Good
+			</button>
+			<button value={3} onClick={handleClick}>
+				Bad
+			</button>
 		</form>
 	);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -27,30 +27,67 @@ export default function Home() {
 			hint: 'O_____'
 		}
 	]);
+	const [nextList, setNextList] = useState<any>([]);
+	let previousList = [];
+
 	const [current, setCurrent] = useState(1);
 	const [total, setTotal] = useState(4);
+	const [complete, setComplete] = useState(false);
 
+	// Move the card to other index
 	function changeCurrent(num: number) {
 		let newNum = current + num;
 		if (newNum < 1) {
 			return;
-		} else if (newNum > total) {
-			return;
 		}
 		setCurrent(newNum);
+	}
+
+	// Add new card to NextList
+	function addToNextList(index: number) {
+		setNextList([...nextList, cardList[index]]);
+	}
+
+	// Switch cardList to nextList
+	function switchList() {
+		previousList = [...cardList];
+		setCurrent(1);
+		setTotal(nextList.length);
+		setCardList([...nextList]);
+		setNextList([]);
+	}
+
+	if (total === 0 && !complete) {
+		setComplete(true);
+	}
+
+	if (current > total && total > 0) {
+		switchList();
 	}
 
 	return (
 		<>
 			<main className="flex flex-col p-4 gap-3 max-w-7xl m-auto">
 				<h1 className="font-semibold text-lg">Geography Midterm</h1>
-				<Card cardList={cardList} current={current} />
-				<RateCard changeCurrent={changeCurrent} />
-				<NavigateCard
-					current={current}
-					total={total}
-					changeCurrent={changeCurrent}
-				/>
+				{!complete ? (
+					<>
+						<Card cardList={cardList} current={current} />
+						<RateCard
+							changeCurrent={changeCurrent}
+							current={current}
+							total={total}
+							addToNextList={addToNextList}
+							switchList={switchList}
+						/>
+						<NavigateCard
+							current={current}
+							total={total}
+							changeCurrent={changeCurrent}
+						/>
+					</>
+				) : (
+					'Complete!'
+				)}
 			</main>
 		</>
 	);
