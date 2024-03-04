89d3bdaaebeaf724c18e452efc2628d3a1549593
Mikael <s.h.koo1213@gmail.com>
2024-03-04 14:48:29 -0500

    add google oauth provider

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,12 +2,27 @@ import prisma from '@/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 // import CredentialsProvider from 'next-auth/providers/credentials';
 import { Adapter } from 'next-auth/adapters';
 
 export const options: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma) as Adapter,
 	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+			profile(profile) {
+				console.log(profile);
+				return {
+					id: profile.sub,
+					name: profile.name,
+					email: profile.email,
+					image: profile.picture,
+					role: profile.role ? profile.role : 'user'
+				};
+			}
+		}),
 		GitHubProvider({
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string,
